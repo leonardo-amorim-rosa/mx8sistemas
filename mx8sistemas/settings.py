650c3d03148c3b907e2f321c564356032c46143a
Leonardo Rosa <leonardo.rosa@mobicare.com.br>
2022-10-10 14:51:40 -0300

    subindo layout da pagina inicial

--- mx8sistemas/settings.py
+++ mx8sistemas/settings.py
@@ -116,9 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
